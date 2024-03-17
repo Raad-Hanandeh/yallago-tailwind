f655e8deff0ec3a38222cc852213db02369c9ccb
Raad-Hanandeh <138323419+Raad-Hanandeh@users.noreply.github.com>
2024-03-18 01:59:47 +0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["./"],
   theme: {
-    extend: {
-      maxWidth: {
-        '1440px': '90rem',
-      },
-      colors: {
-        '#e7e7e6': '#e7e7e6',
-      },
-
-    },
+    extend: {},
   },
   plugins: [],
 }
